dceeeac0a996fbd7886fbc0ca2cbe58068f2ae30
Purushothaman G <officialpurushothaman@gmail.com>
2024-05-14 15:36:13 +0530

    added accounts section in admin side bar

--- src/components/library/admin/navigation/Sidebar.tsx
+++ src/components/library/admin/navigation/Sidebar.tsx
@@ -1,11 +1,20 @@
-import { HStack, Icon, VStack } from "@chakra-ui/react";
-import { Blocks, Eclipse, ShoppingBag, Smile, Snowflake } from "lucide-react";
+import { Button, Divider, HStack, Icon, VStack } from "@chakra-ui/react";
+import {
+  Blocks,
+  CircleUser,
+  Eclipse,
+  LockIcon,
+  LogOut,
+  ShoppingBag,
+  Smile,
+  Snowflake,
+} from "lucide-react";
 import { RHeading } from "../../../Utilities/Typography";
 import SideBarBtn from "./SideBarBtn";
 
 const Sidebar = () => {
   return (
-    <VStack align="start">
+    <VStack align="start" h="100%" pos="relative">
       <HStack color="primary.700" px={3}>
         <Icon as={Snowflake} boxSize={6} />
         <RHeading text="Unicorn" small color="primary.700" />
@@ -15,6 +24,47 @@ const Sidebar = () => {
         <SideBarBtn label="categories" icon={Blocks} />
         <SideBarBtn label="orders" icon={ShoppingBag} />
         <SideBarBtn label="reviews" icon={Smile} />
+      </VStack>
+      <VStack
+        borderRadius={10}
+        w="100%"
+        pos="absolute"
+        bottom={4}
+        bg="primary.50"
+        align="start"
+        p={4}
+        gap={4}
+        boxShadow="sm"
+      >
+        <Button
+          variant="link"
+          leftIcon={<Icon as={CircleUser} />}
+          size="sm"
+          color="primary.600"
+        >
+          Account
+        </Button>
+        <Divider />
+        <Button
+          color="black"
+          variant="link"
+          w="100%"
+          justifyContent="space-between"
+          rightIcon={<Icon as={LockIcon} />}
+          size="sm"
+        >
+          Change Password
+        </Button>
+        <Button
+          colorScheme="red"
+          variant="link"
+          w="100%"
+          justifyContent="space-between"
+          rightIcon={<Icon as={LogOut} />}
+          size="sm"
+        >
+          Logout
+        </Button>
       </VStack>
     </VStack>
   );

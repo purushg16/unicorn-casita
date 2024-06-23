b1f29c44e788998601a356b92fd9abd2d640bdfc
Purushothaman G <officialpurushothaman@gmail.com>
2024-06-23 19:17:33 +0530

    app turned on, landing img updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
-// import router from "./components/router/router";
+import router from "./components/router/router";
 import theme from "./components/theme/theme";
 import "./components/library/user/Embla/embla.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import TempRouter from "./components/router/TempRouter";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={TempRouter} />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
